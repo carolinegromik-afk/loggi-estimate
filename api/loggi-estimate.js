32f0daa94c458c626f22fcfa9f6bcd67fb434d9c
carolinegromik-afk <carolinegromik@gmail.com>
2026-03-31 16:43:59 -0300

    Update loggi-estimate.js

--- api/loggi-estimate.js
+++ api/loggi-estimate.js
@@ -12,31 +12,15 @@ if(!cep){
 return res.status(400).json({error:"cep required"})
 }
 
-const response = await fetch(process.env.LOGGI_API_URL,{
-method:"POST",
-headers:{
-"Content-Type":"application/json",
-"Authorization":`Bearer ${process.env.LOGGI_TOKEN}`
-},
-body:JSON.stringify({
-origin_zip:"09015610",
-destination_zip:cep,
-weight:1,
-length:20,
-width:15,
-height:10
+res.status(200).json({
+days:2
 })
-})
-
-const data = await response.json()
-
-const days = data.delivery_days || data.days || 3
-
-res.status(200).json({days})
 
 }catch(e){
 
-res.status(200).json({days:5})
+res.status(200).json({
+days:5
+})
 
 }
 
